ac1595837ca6023a356c0d580f49ad083223be86
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-04-28 05:18:49 +0000

    feat: Refactor convert_rich_text to handle multiple rich text types and fetch favicons

--- src/client.rs
+++ src/client.rs
@@ -78,116 +78,206 @@ impl Client {
         todo!()
     }
 
-    async fn convert_rich_text(
+    pub async fn convert_rich_text(
         &self,
-        rich_text: notionrs::object::rich_text::RichText,
-    ) -> Result<jarkup_rs::InlineComponent, crate::error::Error> {
-        match rich_text {
-            notionrs::object::rich_text::RichText::Text {
-                text,
-                annotations,
-                plain_text,
-                href,
-            } => {
-                let component = jarkup_rs::Text {
-                    inline: true,
-                    props: jarkup_rs::TextProps {
-                        text: plain_text,
-                        color: match annotations.color {
-                            notionrs::object::color::Color::Default => None,
-                            notionrs::object::color::Color::Blue => Some(String::from("#6987b8")),
-                            notionrs::object::color::Color::Brown => Some(String::from("#8b4c3f")),
-                            notionrs::object::color::Color::Gray => Some(String::from("#868e9c")),
-                            notionrs::object::color::Color::Green => Some(String::from("#59b57c")),
-                            notionrs::object::color::Color::Orange => Some(String::from("#bf7e71")),
-                            notionrs::object::color::Color::Pink => Some(String::from("#c9699e")),
-                            notionrs::object::color::Color::Purple => Some(String::from("#9771bd")),
-                            notionrs::object::color::Color::Red => Some(String::from("#b36472")),
-                            notionrs::object::color::Color::Yellow => Some(String::from("#b8a36e")),
-                            _ => None,
+        rich_text_vec: Vec<notionrs::object::rich_text::RichText>,
+    ) -> Result<Vec<jarkup_rs::InlineComponent>, crate::error::Error> {
+        let mut components: Vec<jarkup_rs::InlineComponent> = Vec::new();
+
+        for rich_text in rich_text_vec {
+            let component: Result<jarkup_rs::InlineComponent, crate::error::Error> = match rich_text
+            {
+                notionrs::object::rich_text::RichText::Text {
+                    text,
+                    annotations,
+                    plain_text,
+                    href,
+                } => {
+                    let component = jarkup_rs::Text {
+                        inline: true,
+                        props: jarkup_rs::TextProps {
+                            text: plain_text,
+                            color: match annotations.color {
+                                notionrs::object::color::Color::Default => None,
+                                notionrs::object::color::Color::Blue => {
+                                    Some(String::from("#6987b8"))
+                                }
+                                notionrs::object::color::Color::Brown => {
+                                    Some(String::from("#8b4c3f"))
+                                }
+                                notionrs::object::color::Color::Gray => {
+                                    Some(String::from("#868e9c"))
+                                }
+                                notionrs::object::color::Color::Green => {
+                                    Some(String::from("#59b57c"))
+                                }
+                                notionrs::object::color::Color::Orange => {
+                                    Some(String::from("#bf7e71"))
+                                }
+                                notionrs::object::color::Color::Pink => {
+                                    Some(String::from("#c9699e"))
+                                }
+                                notionrs::object::color::Color::Purple => {
+                                    Some(String::from("#9771bd"))
+                                }
+                                notionrs::object::color::Color::Red => {
+                                    Some(String::from("#b36472"))
+                                }
+                                notionrs::object::color::Color::Yellow => {
+                                    Some(String::from("#b8a36e"))
+                                }
+                                _ => None,
+                            },
+                            background_color: match annotations.color {
+                                notionrs::object::color::Color::Default => None,
+                                notionrs::object::color::Color::BlueBackground => {
+                                    Some(String::from("#6987b8"))
+                                }
+                                notionrs::object::color::Color::BrownBackground => {
+                                    Some(String::from("#8b4c3f"))
+                                }
+                                notionrs::object::color::Color::GrayBackground => {
+                                    Some(String::from("#868e9c"))
+                                }
+                                notionrs::object::color::Color::GreenBackground => {
+                                    Some(String::from("#59b57c"))
+                                }
+                                notionrs::object::color::Color::OrangeBackground => {
+                                    Some(String::from("#bf7e71"))
+                                }
+                                notionrs::object::color::Color::PinkBackground => {
+                                    Some(String::from("#c9699e"))
+                                }
+                                notionrs::object::color::Color::PurpleBackground => {
+                                    Some(String::from("#9771bd"))
+                                }
+                                notionrs::object::color::Color::RedBackground => {
+                                    Some(String::from("#b36472"))
+                                }
+                                notionrs::object::color::Color::YellowBackground => {
+                                    Some(String::from("#b8a36e"))
+                                }
+                                _ => None,
+                            },
+                            bold: Some(annotations.bold),
+                            italic: Some(annotations.italic),
+                            underline: Some(annotations.underline),
+                            strikethrough: Some(annotations.strikethrough),
+                            katex: None,
+                            code: Some(annotations.code),
+                            ruby: None,
+                            favicon: if let Some(l) = &text.link {
+                                self.fetch_favicon_by_url(&l.url).await
+                            } else {
+                                None
+                            },
+                            href: text.link.map(|l| l.url),
                         },
-                        background_color: match annotations.color {
-                            notionrs::object::color::Color::Default => None,
-                            notionrs::object::color::Color::BlueBackground => {
-                                Some(String::from("#6987b8"))
+                        slots: None,
+                    };
+
+                    Ok(component.into())
+                }
+                notionrs::object::rich_text::RichText::Mention {
+                    mention,
+                    annotations: _annotations,
+                    plain_text,
+                    href: _href,
+                } => {
+                    let component: Result<jarkup_rs::InlineComponent, crate::error::Error> =
+                        match mention {
+                            notionrs::object::rich_text::mention::Mention::User { user: _ } => {
+                                continue;
                             }
-                            notionrs::object::color::Color::BrownBackground => {
-                                Some(String::from("#8b4c3f"))
+                            notionrs::object::rich_text::mention::Mention::Date { date: _ } => {
+                                continue;
                             }
-                            notionrs::object::color::Color::GrayBackground => {
-                                Some(String::from("#868e9c"))
+                            notionrs::object::rich_text::mention::Mention::LinkPreview {
+                                link_preview: _,
+                            } => {
+                                continue;
                             }
-                            notionrs::object::color::Color::GreenBackground => {
-                                Some(String::from("#59b57c"))
+                            notionrs::object::rich_text::mention::Mention::LinkMention {
+                                link_mention,
+                            } => {
+                                let component = jarkup_rs::Text {
+                                    inline: true,
+                                    props: jarkup_rs::TextProps {
+                                        text: plain_text,
+                                        favicon: self
+                                            .fetch_favicon_by_url(&link_mention.href)
+                                            .await,
+                                        href: Some(link_mention.href),
+                                        ..Default::default()
+                                    },
+                                    slots: None,
+                                };
+
+                                let inline_component: jarkup_rs::InlineComponent = component.into();
+
+                                Ok(inline_component)
                             }
-                            notionrs::object::color::Color::OrangeBackground => {
-                                Some(String::from("#bf7e71"))
+                            notionrs::object::rich_text::mention::Mention::TemplateMention {
+                                template_mention: _,
+                            } => {
+                                continue;
                             }
-                            notionrs::object::color::Color::PinkBackground => {
-                                Some(String::from("#c9699e"))
+                            notionrs::object::rich_text::mention::Mention::Page { page: _ } => {
+                                continue;
                             }
-                            notionrs::object::color::Color::PurpleBackground => {
-                                Some(String::from("#9771bd"))
+                            notionrs::object::rich_text::mention::Mention::Database {
+                                database: _,
+                            } => {
+                                continue;
                             }
-                            notionrs::object::color::Color::RedBackground => {
-                                Some(String::from("#b36472"))
+                            notionrs::object::rich_text::mention::Mention::CustomEmoji {
+                                custom_emoji,
+                            } => {
+                                let component = jarkup_rs::Icon {
+                                    inline: true,
+                                    props: jarkup_rs::IconProps {
+                                        src: custom_emoji.url,
+                                        alt: Some(custom_emoji.name),
+                                    },
+                                    slots: None,
+                                };
+
+                                let inline_component: jarkup_rs::InlineComponent = component.into();
+
+                                Ok(inline_component)
                             }
-                            notionrs::object::color::Color::YellowBackground => {
-                                Some(String::from("#b8a36e"))
-                            }
-                            _ => None,
+                        };
+
+                    component
+                }
+                notionrs::object::rich_text::RichText::Equation {
+                    equation,
+                    annotations: _annotations,
+                    plain_text: _plain_text,
+                    href: _href,
+                } => {
+                    let component = jarkup_rs::Text {
+                        inline: true,
+                        props: jarkup_rs::TextProps {
+                            text: equation.expression,
+                            katex: Some(true),
+                            ..Default::default()
                         },
-                        bold: Some(annotations.bold),
-                        italic: Some(annotations.italic),
-                        underline: Some(annotations.underline),
-                        strikethrough: Some(annotations.strikethrough),
-                        katex: None,
-                        code: Some(annotations.code),
-                        ruby: None,
-                        href: text.link.clone().map(|l| l.url),
-                        favicon: if let Some(l) = text.link {
-                            let res = self.reqwest_client.get(l.url).send().await?;
+                        slots: None,
+                    };
 
-                            let html = res.text().await?;
-
-                            let meta_scraper = html_meta_scraper::MetaScraper::new(&html);
-
-                            let favicon = meta_scraper.favicon();
-
-                            favicon
-                        } else {
-                            None
-                        },
-                    },
-                    slots: None,
-                };
-
-                Ok(component.into())
-            }
-            notionrs::object::rich_text::RichText::Mention {
-                mention,
-                annotations,
-                plain_text,
-                href,
-            } => todo!(),
-            notionrs::object::rich_text::RichText::Equation {
-                equation,
-                annotations: _annotations,
-                plain_text: _plain_text,
-                href: _href,
-            } => {
-                let component = jarkup_rs::Text {
-                    inline: true,
-                    props: jarkup_rs::TextProps {
-                        text: equation.expression,
-                        katex: Some(true),
-                        ..Default::default()
-                    },
-                    slots: None,
-                };
-
-                Ok(component.into())
-            }
+                    Ok(component.into())
+                }
+            };
         }
+
+        return Ok(components);
+    }
+
+    pub(crate) async fn fetch_favicon_by_url(&self, url: &str) -> Option<String> {
+        let res = self.reqwest_client.get(url).send().await.ok()?;
+        let html = res.text().await.ok()?;
+        let meta_scraper = html_meta_scraper::MetaScraper::new(&html);
+        meta_scraper.favicon()
     }
 }
