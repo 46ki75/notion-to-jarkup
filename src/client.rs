2da1d3782feebad09485a97cbb02de0a7f961d79
Ikuma Yamashita <me@ikuma.cloud>
2026-03-26 02:26:27 +0900

    feat: add image size fetching capability to client

--- src/client.rs
+++ src/client.rs
@@ -12,6 +12,8 @@ pub struct Client {
     /// If true, unsupported blocks will be rendered as `Unsupported` blocks.
     /// If false, unsupported blocks will be skipped.
     pub enable_unsupported_block: bool,
+
+    pub enable_fetch_image_size: bool,
 }
 
 impl Client {
@@ -466,11 +468,34 @@ impl Client {
                         _ => Some(String::from("untitled")),
                     };
 
+                    let (width, height) = if self.enable_fetch_image_size {
+                        let image_bytes = self
+                            .reqwest_client
+                            .get(image.get_url())
+                            .header("User-Agent", "notion-to-jarkup")
+                            .send()
+                            .await?
+                            .bytes()
+                            .await?;
+
+                        let info = image::ImageReader::new(std::io::Cursor::new(image_bytes))
+                            .with_guessed_format()
+                            .ok()
+                            .and_then(|reader| reader.into_dimensions().ok())
+                            .map_or((None, None), |(x, y)| (Some(x), Some(y)));
+
+                        info
+                    } else {
+                        (None, None)
+                    };
+
                     let component = jarkup_rs::Image {
                         id: Some(block.id),
                         props: jarkup_rs::ImageProps {
                             src: image.get_url(),
                             alt: maybe_caption,
+                            width,
+                            height,
                             ..Default::default()
                         },
                         slots: None,
